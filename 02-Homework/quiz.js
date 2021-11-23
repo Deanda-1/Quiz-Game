2e868fa76f94921549382b0e183f8d84ddf89a29
Deanda Noble <82609969+Deanda-1@users.noreply.github.com>
2021-11-23 08:32:00 -0500

    Update quiz.js
    Potential final update including logic for the quiz, scoreboard, and utility functions.

--- 02-Homework/quiz.js
+++ 02-Homework/quiz.js
@@ -1,24 +1,55 @@
-const startButton = docunment.gitElementById("start-btn")
-const nextButton = docunment.gitElementById("next-btn")
-const questionContainerElement = docunment.gitElementById("question-container")
-const questionElement = document.gitElementById('question')
+// Button Constants
+const startButton = document.getElementById("start-button")
+const nextButton = document.getElementById("next-button")
+const questionContainerElement = document.getElementById("question-container")
+const questionElement = document.getElementById('question')
 const answerButtonElement = document.getElementById('answer-button')
+const scoreBoardElement = document.getElementById('scoreboard')
 
-let shuffledQuestions,currectQuestionIndex;
+// Score Constants & Variables
+const scoreBar = document.getElementById("right-answers");
+const timeBar = document.getElementById("timer");
+let scoreBoard = JSON.parse(localStorage.getItem("quizGame-scoreboard"))
+let timer = 90;
+let timerState = false;
+let timerCallback;
+
+// Logic Variables
+let shuffledQuestions, currectQuestionIndex;
 let quizScore = 0;
 
-startButton.addEventListener('click',startGame)
+startButton.addEventListener('click', newGame)
 nextButton.addEventListener('click', () => {
-  correctQuestionIndex++
+  correctQuestionIndex++;
+  setNextQuestion();
 })
 
 function newGame() {
   startButton.classList.add('hide')
-  shuffledQuestions = questions.sort(() => math.random() -0.5)
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5)
   correctQuestionIndex = 0;
   questionContainerElement.classList.remove('hide')
-  setNextQuestion()
-  quizScore = 0
+  setNextQuestion();
+  quizScore = 0;
+  scoreBar.innerText = "Score: " + quizScore;
+  timerState = true;
+  timer = 90;
+  timerCallback = setInterval(function () {
+    timeBar.innerText = "Remaining Time: " + timer;
+
+    // if timerState is false, unregister callback
+    if (!timerState) {
+      clearInterval(timerCallback)
+    }
+
+    // if game is running...
+    if (timer <= 0) {
+      gameover();
+      //timerState = false;
+    } else {
+      timer--;
+    }
+  }, 1000) // call this routine once every second
 }
 
 function setNextQuestion() {
@@ -27,100 +58,145 @@ function setNextQuestion() {
 }
 
 function showQuestion(question) {
-  questionElement.innertext = question.question;
-  question.answer.forEach((answer) => {
-    const button = document.createElement('button') 
-    button.innerText = answer.text;
+  console.log(question);
+  questionElement.innerText = question.question;
+  question.options.forEach((answer) => {
+    const button = document.createElement('button')
+    button.innerText = answer;
     button.classList.add('btn')
-    if(answer.correct) {
-      button.dataset.correct = answer.correct
+    if (answer == question.answer) {
+      button.dataset.correct = question.answer
     }
-    button.addEventListener('click',selectAnswer)
-    answerButtonsElement.appendChild(button)
+    button.addEventListener('click', selectAnswer)
+    answerButtonElement.appendChild(button)
   })
 }
 
 function resetState() {
   clearStatusClass(document.body)
   nextButton.classList.add('hide')
-  while(answerButtonsElement.firstChild) {
-    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
-   }
+  while (answerButtonElement.firstChild) {
+    answerButtonElement.removeChild(answerButtonElement.firstChild)
+  }
 }
 
 function selectAnswer(e) {
-  const selectedButton = e.target 
-  const correct = selectrButton.dataset.correct
-  
-   setStatusClass(document.body,correct)
-    Array.from(answerButtonsElement.children).forEach((button)=>{
-    })
-  if(shuffledQuestions.lenght > correctQuestionIndex +1) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+
+  console.log(selectedButton.innerText);
+  console.log(shuffledQuestions[correctQuestionIndex].answer);
+
+  if (selectedButton.innerText == shuffledQuestions[correctQuestionIndex].answer) {
+    setStatusClass(document.body, true);
+    quizScore++;
+  } else {
+    setStatusClass(document.body, false);
+    timer = timer - 5;
+  }
+  scoreBar.innerText = "Score: " + quizScore;
+
+  // Array.from(answerButtonElement.children).forEach((button) => {
+  // })
+
+
+  // if quiz is NOT over - show next button
+  if (shuffledQuestions.length > correctQuestionIndex + 1) {
     nextButton.classList.remove("hide")
   } else {
-    startButton.innerText = "restert"
+    // otherwise, offer to restart
+    gameover();
+    startButton.innerText = "restart"
     startButton.classList.remove("hide")
   }
-  if(selectedButton.dataset = correct) {
-    quizScore++
-  } 
 }
 
-function setStatusClass(element,correct) {
-  if(correct) {
-    element.classList.add("correct")
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add("correct");
   } else {
-    element.classList.add("wrong")  
-  } 
+    element.classList.add("wrong");
+  }
 }
 
 function clearStatusClass(element) {
-  element.classList.remove('correct')
-  element.classList.remove('wrong')
+  element.classList.remove('correct');
+  element.classList.remove('wrong');
 }
-let questions = [
+
+function gameover() {
+  timerState = false;
+  scoreBar.innerText = "Final Score: " + quizScore;
+  const initials = prompt("Please enter your initials for the highscore table");
+  if (!scoreBoard){ scoreBoard=[]; } // make sure scoreboard exists
+  scoreBoard.push(
     {
-        numb: 1,
-        question: "Which one of these is a JavaScript framework?",
-        answer: "React",
-        options: [
-            "Python",
-            "Django",
-            "React",
-            "Eclipse"
-        ]
-    },
-    {
-        numb: 2,
-        question: "Which one of these is a css style?",
-        answer: "Font",
-        options: [
-            "Font",
-            "Function",
-            "Bike",
-            "Fish"
-        ]
-    },
-    {
-        numb: 3,
-        question: "Which one of these is an HTML element?",
-        answer: "Link",
-        options: [
-            "Font",
-            "Family",
-            "Car",
-            "Link"
-        ]
-    },
-    {
-        numb: 4,
-        question: "Which one of these is a Jquery?",
-        answer: "$",
-        options: [
-            "Rocket",
-            "$",
-            "Mountain",
-            "Monkey"
-        ]
+      name: initials,
+      score: quizScore
     }
+  );
+
+  scoreBoard.sort((a,b) => (a.score < b.score)?1:-1);
+
+  localStorage.setItem("quizGame-scoreboard", JSON.stringify(scoreBoard));
+
+  //TODO: Show Highscore Table
+  showScoreBoard();
+}
+
+function showScoreBoard (){
+  for (i=0; i< scoreBoard.length; i++){
+    line = document.createElement('li');
+    line.innerText = scoreBoard[i].name + " - " + scoreBoard[i].score;
+    scoreBoardElement.appendChild(line);
+  }
+
+}
+
+let questions = [
+  {
+    numb: 1,
+    question: "Which one of these is a JavaScript framework?",
+    answer: "React",
+    options: [
+      "Python",
+      "Django",
+      "React",
+      "Eclipse"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Which one of these is a css style?",
+    answer: "Font",
+    options: [
+      "Font",
+      "Function",
+      "Bike",
+      "Fish"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Which one of these is an HTML element?",
+    answer: "Link",
+    options: [
+      "Font",
+      "Family",
+      "Car",
+      "Link"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Which one of these is a Jquery?",
+    answer: "$",
+    options: [
+      "Rocket",
+      "$",
+      "Mountain",
+      "Monkey"
+    ]
+  }
 ]
